9a313098ded38402cfef22d225c6ebf4c2641fa5
Cavin <cavinlarry001@gmail.com>
2026-06-22 02:25:16 +0300

    Added holder test

--- backend/src/test/kotlin/io/github/devcavin/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/io/github/devcavin/backend/BackendApplicationTests.kt
@@ -1,13 +1,16 @@
 package io.github.devcavin.backend
 
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BackendApplicationTests {
+    private val logger = LoggerFactory.getLogger(BackendApplicationTests::class.java)
 
     @Test
     fun contextLoads() {
+        logger.info("Application context loaded successfully")
     }
 
 }
